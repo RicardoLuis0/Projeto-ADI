1476261a13be30a781806c8f6a04de89519e4f48
Ricardo <ricolvs123@gmail.com>
2019-09-03 20:52:15 -0300

    Update DBUser.java

--- src/main/java/com/ricardoluis/db/data/DBUser.java
+++ src/main/java/com/ricardoluis/db/data/DBUser.java
@@ -13,7 +13,7 @@ public class DBUser {
 	@Column(name="id")
 	private int id;
 	
-	@Column(name="name")
+	@Column(name="name",unique=true)
 	private String name;
 	
 	@Column(name="birthdate")
@@ -23,6 +23,14 @@ public class DBUser {
 		this.id=id;
 		this.name = name;
 		this.birthdate = birthdate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 	public String getName() {
