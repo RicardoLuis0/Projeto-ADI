cf20c4ba31d4e98477938eb47415162b35ed6e08
Ricardo <ricolvs123@gmail.com>
2019-09-10 20:14:22 -0300

    add PreparedStatement support to JDBCConnector

--- src/main/java/com/ricardoluis/jdbc/JDBCConnector.java
+++ src/main/java/com/ricardoluis/jdbc/JDBCConnector.java
@@ -6,6 +6,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 
 public abstract class JDBCConnector{//template method pattern
 
@@ -14,35 +15,60 @@ public abstract class JDBCConnector{//template method pattern
 
 	@FunctionalInterface
 	public interface RSGetter<T>{
-		T get(ResultSet rs) throws SQLException;
+		T getObject(ResultSet rs) throws SQLException;
+	}
+
+	@FunctionalInterface
+	public interface StatementBuilder{
+		PreparedStatement getStatement(Connection conn) throws SQLException;
 	}
 
 	private static <T> ArrayList<T> RSToArrayList(ResultSet rs,RSGetter<T> getter) throws SQLException {
 		ArrayList<T> result=null;
 		while(rs.next()) {
 			if(result==null) result=new ArrayList<T>();
-			result.add(getter.get(rs));
+			result.add(getter.getObject(rs));
 		}
 		return result;
 	}
 
 	public final int executeUpdate(String sql) throws SQLException {
-		Connection c=openConnection();
-		try(Statement stmt=c.createStatement()){
+		Connection conn=openConnection();
+		try(Statement stmt=conn.createStatement()){
 			return stmt.executeUpdate(sql);
 		}finally {
-			closeConnection(c);
+			closeConnection(conn);
+		}
+	}
+
+	public final int executeUpdate(StatementBuilder builder) throws SQLException {
+		Connection conn=openConnection();
+		try(PreparedStatement stmt=builder.getStatement(conn)){
+			return stmt.executeUpdate();
+		}finally {
+			closeConnection(conn);
 		}
 	}
 
 	public final <T> ArrayList<T> executeQuery(String sql, RSGetter<T> getter) throws SQLException {
-		Connection c=openConnection();
-		try(Statement stmt=c.createStatement()){
+		Connection conn=openConnection();
+		try(Statement stmt=conn.createStatement()){
 			try(ResultSet rs=stmt.executeQuery(sql)){
 				return RSToArrayList(rs, getter);
 			}
 		}finally {
-			closeConnection(c);
+			closeConnection(conn);
+		}
+	}
+
+	public final <T> ArrayList<T> executeQuery(StatementBuilder builder, RSGetter<T> getter) throws SQLException {
+		Connection conn=openConnection();
+		try(PreparedStatement stmt=builder.getStatement(conn)){
+			try(ResultSet rs=stmt.executeQuery()){
+				return RSToArrayList(rs, getter);
+			}
+		}finally {
+			closeConnection(conn);
 		}
 	}
 
